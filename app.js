bc137fca65b676082b56246d4652d6a8d617d55e
atheeswaran-g <aatheeswarangovindraj@gmail.com>
2025-07-10 00:00:13 +0530

    app.js: change in message

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const name = "Sethucode";
 
 app.get("/", (req, res) => {
-  res.send("Welcome to ShopEasy CodeTest!");
+  const message = `Welcome to ShopEasy from ${name}!`
+  res.send(message);
 });
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-module.exports = {app,server}
+module.exports = {app,server,name}
